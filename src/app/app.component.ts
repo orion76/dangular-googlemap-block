68550b5971761b78a2120beb5d3aa94e690eb011
Pasha <gpb@yandex.ru>
2019-10-29 14:22:19 +0000

    build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,6 @@ import {Component, OnInit} from '@angular/core';
                     draggable="true"
                     positionTop="0"
                     [positionLeft]="positionLeft"
-                    appendTo="google-map"
                     [autoZIndex]="autoZIndex"
           >
               <terminal-filter-form class="terminal-filter-form map-block-item"></terminal-filter-form>
